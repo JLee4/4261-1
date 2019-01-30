2b3a3307a98ad436cc341b2707f231226187a1d8
Nick <nsenador1@gmail.com>
2019-01-30 00:51:38 -0500

    Change main activity to tell you whether it is light or dark outside

--- app/src/main/java/assignment1/assignment1/MainActivity.java
+++ app/src/main/java/assignment1/assignment1/MainActivity.java
@@ -1,14 +1,47 @@
 package assignment1.assignment1;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient mFusedLocationClient;
+    private ConstraintLayout mConstraintLayout;
+    private TextView textView;
+    String appid = "9895c75579e2599cca1a74c9799575e5";
+    private double latitude;
+    private double longitude;
+    private Long sunrise;
+    private Long sunset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +55,115 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        mConstraintLayout = findViewById(R.id.container);
+        textView = findViewById(R.id.text1);
+        getLocation();
+    }
+
+    private void analyzeDarkness() {
+        Date current = new Date(System.currentTimeMillis());
+        Log.d("currentTime", Long.toString(current.getTime()));
+        Date rise = new Date(sunrise);
+        Date set = new Date(sunset);
+        if (current.after(rise) && current.before(set)) {
+            textView.setText("It is light outside.");
+            mConstraintLayout.setBackgroundColor(0);
+        }
+    }
+
+    private void getWeather() {
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://api.openweathermap.org/data/2.5/weather?lat="
+                + latitude + "&lon=" + longitude + "&appid=" + appid;
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        try {
+                            // parsing file "JSONExample.json"
+                            Object obj = new JSONParser().parse(response);
+
+                            // typecasting obj to JSONObject
+                            JSONObject jo = (JSONObject) obj;
+                            Log.d("response", jo.toString());
+
+                            JSONObject sys  = (JSONObject) jo.get("sys");
+                            sunrise = ((Long) sys.get("sunrise")) * 1000;
+                            sunset = ((Long) sys.get("sunset")) * 1000;
+                            Log.d("sunrise", sunrise.toString());
+                            Log.d("sunset", sunset.toString());
+
+                            analyzeDarkness();
+
+                        } catch (Exception e) {
+                            Log.e("error", e.getMessage());
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+    private void getLocation() {
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                new AlertDialog.Builder(this)
+                        .setTitle("Location permissions")
+                        .setMessage("This app requires location permissions")
+                        .setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                        0);
+                            }
+                        })
+                        .create()
+                        .show();
+
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        0);
+            }
+        }
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        latitude = location.getLatitude();
+                        longitude = location.getLongitude();
+                        Log.d("latitude", Double.toString(latitude));
+                        Log.d("longitude", Double.toString(longitude));
+                        getWeather();
+                        if (location != null) {
+                            // Logic to handle location object
+                        }
+                    }
+                });
     }
 
     @Override
